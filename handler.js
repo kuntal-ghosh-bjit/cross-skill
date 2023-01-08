b532233f516dc3745f27917ffa992798d19a35ac
DESKTOP-HBI9H3K\Kuntal <kuntal.ghosh@bjitgroup.com>
2023-01-08 23:22:43 +0600

    modified order api

--- handler.js
+++ handler.js
@@ -8,8 +8,33 @@ const app = express();
 const USERS_TABLE = process.env.USERS_TABLE;
 console.log("users",USERS_TABLE);
 const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+// const dynamoDbClientParams = {};
+// if (process.env.IS_OFFLINE) {
+//   dynamoDbClientParams.region = 'localhost'
+//   dynamoDbClientParams.endpoint = 'http://localhost:8000'
+// }
 
 app.use(express.json());
+
+app.get("/users", async function (req, res) {
+  const params = {
+    TableName: USERS_TABLE,
+  };
+
+  try {
+    const { Item } = await dynamoDbClient.get(params).promise();
+    if (Item) {
+      res.json(Item);
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find user with provided "userId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retreive user" });
+  }
+});
 
 app.get("/users/:userId", async function (req, res) {
   const params = {
@@ -22,8 +47,8 @@ app.get("/users/:userId", async function (req, res) {
   try {
     const { Item } = await dynamoDbClient.get(params).promise();
     if (Item) {
-      const { userId, name } = Item;
-      res.json({ userId, name });
+      const {  userId, name,orderId ,firstName,lastName,address,productName,quantity } = Item;
+      res.json({  userId, name,orderId ,firstName,lastName,address,productName,quantity });
     } else {
       res
         .status(404)
@@ -35,8 +60,9 @@ app.get("/users/:userId", async function (req, res) {
   }
 });
 
-app.post("/users", async function (req, res) {
-  const { userId, name } = req.body;
+
+app.post("/orders", async function (req, res) {
+  const { userId, name,orderId ,firstName,lastName,address,productName,quantity } = req.body;
   if (typeof userId !== "string") {
     res.status(400).json({ error: '"userId" must be a string' });
   } else if (typeof name !== "string") {
@@ -48,12 +74,18 @@ app.post("/users", async function (req, res) {
     Item: {
       userId: userId,
       name: name,
+      orderId:orderId,
+      firstName:firstName,
+      lastName:lastName,
+      address:address,
+      productName:productName,
+      quantity:quantity
     },
   };
 
   try {
     await dynamoDbClient.put(params).promise();
-    res.json({ userId, name });
+    res.json({ userId, name,orderId ,firstName,lastName,address,productName,quantity });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Could not create user" });
